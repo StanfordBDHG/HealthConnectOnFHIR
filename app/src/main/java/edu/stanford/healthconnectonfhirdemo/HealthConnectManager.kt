2d84c90b3e97b295d02c3a493e5e99ea769bbb2a
Vishnu Ravi <vishnur@stanford.edu>
2024-05-22 11:00:03 -0400

    Set up main screen

--- app/src/main/java/edu/stanford/healthconnectonfhirdemo/HealthConnectManager.kt
+++ app/src/main/java/edu/stanford/healthconnectonfhirdemo/HealthConnectManager.kt
@@ -1,8 +1,10 @@
 package edu.stanford.healthconnectonfhirdemo
 
 import android.content.Context
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.compose.runtime.mutableStateOf
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.PermissionController
 import androidx.health.connect.client.records.StepsRecord
 import androidx.health.connect.client.records.WeightRecord
 import androidx.health.connect.client.request.ReadRecordsRequest
@@ -26,6 +28,15 @@ class HealthConnectManager(private val context: Context) {
         return availabilityStatus == HealthConnectClient.SDK_AVAILABLE
     }
 
+    suspend fun hasAllPermissions(permissions: Set<String>): Boolean {
+        return healthConnectClient?.permissionController?.getGrantedPermissions()?.containsAll(permissions)
+            ?: false
+    }
+
+    fun requestPermissionsActivityContract(): ActivityResultContract<Set<String>, Set<String>> {
+        return PermissionController.createRequestPermissionResultContract()
+    }
+
     suspend fun getStepsRecords(startTime: Instant, endTime: Instant): List<StepsRecord>? {
         return healthConnectClient?.readRecords(
                 ReadRecordsRequest(
@@ -33,5 +44,19 @@ class HealthConnectManager(private val context: Context) {
                     timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
                 )
             )?.records
+    }
+
+    suspend fun writeStepsRecord(stepsCount: Long) {
+        val now = Instant.now()
+        val zoneOffset = java.time.ZoneId.systemDefault().rules.getOffset(now)
+
+        val stepsRecord = StepsRecord(
+            count = stepsCount,
+            startTime = now,
+            endTime = now,
+            startZoneOffset = zoneOffset,
+            endZoneOffset = zoneOffset
+        )
+        healthConnectClient?.insertRecords(listOf(stepsRecord))
     }
 }
